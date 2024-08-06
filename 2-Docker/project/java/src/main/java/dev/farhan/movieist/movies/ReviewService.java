b1461ac386a41bec29545bd244bc32f8efd6884e
adesinna <aladesaea@yahoo.com>
2024-08-06 18:53:28 -0400

    debug

--- 2-Docker/project/java/src/main/java/dev/farhan/movieist/movies/ReviewService.java
+++ 2-Docker/project/java/src/main/java/dev/farhan/movieist/movies/ReviewService.java
@@ -22,8 +22,9 @@ public class ReviewService {
 
         mongoTemplate.update(Movie.class)
             .matching(Criteria.where("imdbId").is(imdbId))
-                .apply(new Update().push("reviewIds").value(review.getId()))
-                .first();
+            .apply(new Update().push("reviews").value(review))
+            .first();
+
         return review;
     }
 }
